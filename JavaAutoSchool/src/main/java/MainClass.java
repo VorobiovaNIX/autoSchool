5e2b925f83d650b0b4f789291d1009995d0e7496
avorobyova <vorobbyova@gmail.com>
2020-05-27 12:31:09 +0300

    Added solution for task 5.4-stream API.

--- JavaAutoSchool/src/main/java/MainClass.java
+++ JavaAutoSchool/src/main/java/MainClass.java
@@ -1,36 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+
+    /* Создайте список List<String> не короче 50 элементов разной длинны (наполнение не важно).
+    Добавьте вручную несколько пустых элементов ""
+    С помощью stream() выведите на консоль:
+    - длинну каждого из элементов списка.
+    - не пустые элементы списка не содержащие символ d
+    - элементы списка в алфавитном порядке. */
 
 public class MainClass {
     public static void main(String[] args) {
 
-
-    }
-
-    /*  Создайте список List<String> не короче 50 элементов разной длинны (наполнение не важно).
-Добавьте вручную несколько пустых элементов ""
-С помощью stream() выведите на консоль:
-- длинну каждого из элементов списка.
-- не пустые элементы списка не содержащие символ d
-- элементы списка в алфавитном порядке. */
-
-    static void printByStream() {
-
         List<String> list = new ArrayList<>();
 
-        for (int i = 0; i <= 10; i++) {
-            //Lorem Ipsum is simply dummy
+        IntStream.range(1, 11).forEach((i) -> {
             list.add("text" + i);
             list.add("of the printing and typesetting industry" + i);
             list.add("Lorem Ipsum has been the industry" + i);
             list.add("It has survived not only five centuries" + i);
             list.add("but also the leap into electronic typesetting" + i);
-        }
+        });
 
         list.add("");
         list.add("");
-
-        list.stream();
         list.forEach(System.out::println);
+
+        list.stream().map(String::length).forEach(System.out::println);
+
+        list.stream().filter(x -> x.length() > 0).filter(x -> !x.contains("d")).forEach(System.out::println);
+
+        list.stream().sorted().forEach(System.out::println);
     }
 }
