1db76087c4cbe49999f923017bba97bd8991bc86
avorobyova <vorobbyova@gmail.com>
2020-06-24 15:04:15 +0300

    Added request spec.

--- JavaAutoSchool/src/test/java/ApiAutoRiaTest.java
+++ JavaAutoSchool/src/test/java/ApiAutoRiaTest.java
@@ -1,5 +1,7 @@
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.builder.RequestSpecBuilder;
 import com.jayway.restassured.builder.ResponseSpecBuilder;
+import com.jayway.restassured.specification.RequestSpecification;
 import com.jayway.restassured.specification.ResponseSpecification;
 import org.json.JSONException;
 import org.junit.After;
@@ -11,22 +13,24 @@ import static com.jayway.restassured.RestAssured.given;
 
 public class ApiAutoRiaTest {
 
-    ResponseSpecBuilder spec;
     ResponseSpecification responseSpec;
+    RequestSpecification requestSpec;
 
     @Before
     public void setup() {
         RestAssured.baseURI = "https://auto.ria.com";
-        spec = new ResponseSpecBuilder();
-        spec.expectStatusCode(200);
-        spec.expectContentType("text/html");
-        spec.expectHeader("Content-Encoding", "gzip");
-        responseSpec = spec.build();
+        responseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType("text/html")
+                .expectHeader("Content-Encoding", "gzip").build();
+
+        requestSpec = new RequestSpecBuilder()
+                .addHeader("User-Agent", "Jmeter").build();
     }
 
     @Test
     public void testMitsubishi() throws JSONException {
-        given().log().all().header("User-Agent", "Jmeter")
+        given().log().all().spec(requestSpec)
                 .when()
                 .get("/auto_mitsubishi_lancer_21990745.html")
                 .then()
@@ -36,7 +40,7 @@ public class ApiAutoRiaTest {
 
     @Test
     public void testToyota() throws JSONException {
-        given().header("User-Agent", "Jmeter")
+        given().spec(requestSpec)
                 .when()
                 .get("/auto_toyota_camry_22101863.html")
                 .then()
@@ -46,7 +50,7 @@ public class ApiAutoRiaTest {
 
     @Test
     public void testBmw() throws JSONException {
-        given().header("User-Agent", "Jmeter")
+        given().spec(requestSpec)
                 .when()
                 .get("/auto_bmw_520_21996828.html")
                 .then()
@@ -56,7 +60,7 @@ public class ApiAutoRiaTest {
 
     @Test
     public void testNissanLeaf() throws JSONException {
-        given().header("User-Agent", "Jmeter")
+        given().spec(requestSpec)
                 .when()
                 .get("/auto_nissan_leaf_22104675.html")
                 .then()
@@ -66,7 +70,7 @@ public class ApiAutoRiaTest {
 
     @Test
     public void testNissanLeaf2() throws JSONException {
-        given().header("User-Agent", "Jmeter")
+        given().spec(requestSpec)
                 .when()
                 .get("/auto_nissan_leaf_22104647.html")
                 .then()
@@ -76,7 +80,7 @@ public class ApiAutoRiaTest {
 
     @Test
     public void testMercedesBenz() throws JSONException {
-        given().header("User-Agent", "Jmeter")
+        given().spec(requestSpec)
                 .when()
                 .get("/auto_mercedes_benz_e_220_21994701.html")
                 .then()
@@ -86,7 +90,7 @@ public class ApiAutoRiaTest {
 
     @Test
     public void testLexusNx() throws JSONException {
-        given().header("User-Agent", "Jmeter")
+        given().spec(requestSpec)
                 .when()
                 .get("/auto_lexus_nx_200_22083607.html")
                 .then()
---
+++ JavaAutoSchool/src/test/java/JsonPathTask.java
@@ -0,0 +1,11 @@
+public class JsonPathTask {
+
+
+    //File json = new File(String "/src/test/resources/JsonExample.txt");
+    //private JsonPath jp = new JsonPath(json); //JsonPath object
+
+//    @Test
+//    public void testJsonPath() {
+//        jp.get();
+//    }
+}
